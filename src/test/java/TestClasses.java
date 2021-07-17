a51a0774534a0ce3567560cbf8a39f44e9daa693
acarlescu2 <carlescu.alexandru@gmail.com>
2021-07-17 10:34:44 +0300

    Push

---
+++ src/test/java/TestClasses.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+public class TestClasses {
+    @Test
+     public void Test01(){
+        System.out.println("This is my first test");
+    }
+}
---
+++ src/test/java/TestNgClasses.java
@@ -0,0 +1,8 @@
+import org.testng.annotations.Test;
+
+public class TestNgClasses {
+    @Test
+    public void Test01(){
+        System.out.println("my first test TestNG test!");
+    }
+}
